b0f8b22432d53e4b3ac61a1fff1358f515367d0e
Riku Leskinen <rleskine@c1r6p3.hive.fi>
2023-02-04 22:28:15 +0200

    Timer variation and Rootboss scene added

--- MobSpawn.gd
+++ MobSpawn.gd
@@ -5,17 +5,67 @@ var carrot_spd_min = 250.0
 var beet_spd_min = 150.0
 var beet_spd_max = 250.0
 
+var level = 1
+var levelmobs = 5
+var mobsspawned = 0
+var mobsperlevel = 1
+var bosstimer = 15.0
+var fulltimer = 3.0
+var decrease = 0.25
+
+func _ready():
+	$Mobtimer.wait_time = fulltimer
+	$Mobtimer.start()
+
+func increase_timer():
+	if (mobsspawned < levelmobs):
+		$Mobtimer.stop()
+		$Mobtimer.wait_time = fulltimer - ((fulltimer/1.5) * (float(mobsspawned)/levelmobs))
+		$Mobtimer.start()
+		mobsspawned += 1
+	else:
+		$Mobtimer.stop()
+		$Mobtimer.wait_time = bosstimer - (decrease * level)
+		$Mobtimer.start()
+		mobsspawned = 0
+		levelmobs += mobsperlevel
+		mobsperlevel += 1
+		fulltimer -= decrease
+		level += 1
+		print (level)
+	print ($Mobtimer.wait_time)
+
 func _on_Mobtimer_timeout():
+	increase_timer()
+	spawnEnemy()
+	var adds = level
+	while (adds > 3):
+		spawnEnemy()
+		adds -= 3
+
+func spawnEnemy():
 	var rand = RandomNumberGenerator.new()
 	var screen_size = get_viewport().get_visible_rect().size
 	var enemyscene
+	var enemy
 	var direction
+	var velocity
 	var vegtype = randi()%3
+	#increase_timer()
+	if (level > 1 and mobsspawned == 0):
+		enemyscene = load("res://RootBoss.tscn")
+		enemy = enemyscene.instance()
+		enemy.position.y = -200
+		enemy.position.x = screen_size.x/2 - 125
+		direction = 0
+		enemy.linear_velocity = Vector2(0, 50 + (level * 10))
+		add_child(enemy)
+		return
 	if (vegtype == 2):
 		enemyscene = load("res://Potato.tscn")
 	else:
 		enemyscene = load("res://RootVeg.tscn")
-	var enemy = enemyscene.instance()
+	enemy = enemyscene.instance()
 	if (vegtype < 2):
 		enemy.settype(vegtype)
 		rand.randomize()
@@ -30,10 +80,9 @@ func _on_Mobtimer_timeout():
 			enemy.position.x = -30
 			direction = PI /2
 		else:
-			enemy.position.x = screen_size.x - 60
+			enemy.position.x = screen_size.x + 30
 			direction = -PI /2
 		enemy.position.y = rand_range(screen_size.y/8, (screen_size.y * 3)/4)
-	var velocity
 	if (vegtype == 0):
 		if (enemy.position.x > (screen_size.x / 2)):
 			direction = rand_range(PI / 8, PI / 4)
@@ -49,7 +98,6 @@ func _on_Mobtimer_timeout():
 		if (enemy.position.x < 0):
 			enemy._direction = 1
 			enemy.get_node("Potato").flip_v = true
-			enemy.get_node("Potato").play("", true)
 		elif (enemy.position.x > 0):
 			enemy._direction = 0 
 			enemy.get_node("Potato").play("", true)
---
+++ RootBoss.gd
@@ -0,0 +1,7 @@
+extends RigidBody2D
+
+func _ready():
+	pass # Replace with function body.
+
+func _on_VisibilityNotifier2D_screen_exited():
+	pass # Replace with function body.
