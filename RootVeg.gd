20909c06c634f6694d8156e6638f2e221d03bf96
Riku Leskinen <rleskine@c1r6p3.hive.fi>
2023-02-04 20:13:59 +0200

    Fixed bugs in rootveg.gd and started potato anim

--- RootVeg.gd
+++ RootVeg.gd
@@ -6,29 +6,20 @@ func settype(type):
 	 vegtype = type
 
 func _ready():
-	var rootvegtype = randi()%3
-	if (rootvegtype == 1):
+	if (vegtype == 0):
 		$Beet.playing = true
 		$Carrot.playing = false
 		$Carrot.visible = false
 		$Potato.visible = false
 		$Potato.playing = false
 		$BeetCollision.disabled = false
-	elif (rootvegtype == 2):
+	elif (vegtype == 1):
 		$Beet.playing = false
 		$Beet.visible = false
 		$Carrot.playing = true
 		$Potato.visible = false
 		$Potato.playing = false
 		$CarrotCollision.disabled = false
-	elif (rootvegtype == 0):
-		$Beet.playing = false
-		$Beet.visible = false
-		$Carrot.playing = false
-		$Carrot.visible = false
-		$Potato.visible = true
-		$Potato.playing = true
-		$PotatoCollision.disabled = false
 	
 func _on_VisibilityNotifier2D_screen_exited():
 	queue_free()
