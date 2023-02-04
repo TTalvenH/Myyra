428c290847f1e6f440fb2f37a5f5f99cb39ec8e2
Riku Leskinen <rleskine@c1r6p3.hive.fi>
2023-02-04 19:59:34 +0200

    Modified MobSpawn.gd, RootVeg.gd, added Potato scene

--- MobSpawn.gd
+++ MobSpawn.gd
@@ -1,42 +1,53 @@
 extends Node2D
 
-var root_spd_max = 300.0
-var root_spd_min = 200.0
+var carrot_spd_max = 350.0
+var carrot_spd_min = 250.0
+var beet_spd_min = 150.0
+var beet_spd_max = 250.0
 
 func _on_Mobtimer_timeout():
-    var rand = RandomNumberGenerator.new()
-    var Enemyscene = load("res://RootVeg.tscn")
-    var screen_size = get_viewport().get_visible_rect().size
-
-    var enemy = Enemyscene.instance()
-    rand.randomize()
-    var x = rand.randf_range(0, screen_size.x)
-    rand.randomize()
-    var y = 10
-
-    enemy.position.y = y
-    enemy.position.x = x
-    var direction
-    if (enemy.position.x > (screen_size.x / 2)):
-        direction = rand_range(0, PI /4)
-    else:
-        direction = rand_range(-PI / 4, 0)
-    var velocity = Vector2(0, rand_range(root_spd_min, root_spd_max))
-    enemy.linear_velocity = velocity.rotated(direction)
-    add_child(enemy)
-
-#func _on_Mobtimer_timeout():
-#	var rand = RandomNumberGenerator.new()
-#	var Enemyscene = load("res://Enemy.tscn")
-#	var screen_size = get_viewport().get_visible_rect().size
-#	
-#	var enemy = Enemyscene.instance()
-#	rand.randomize()
-#	var x = rand.randf_range(0, screen_size.x)
-#	rand.randomize()
-#	var y = 10
-#	
-#	enemy.position.y = y
-#	enemy.position.x = x
-#	add_child(enemy)
-
+	var rand = RandomNumberGenerator.new()
+	var screen_size = get_viewport().get_visible_rect().size
+	var enemyscene
+	var direction
+	var vegtype = randi()%3
+	if (vegtype == 2):
+		enemyscene = load("res://Potato.tscn")
+	else:
+		enemyscene = load("res://RootVeg.tscn")
+	var enemy = enemyscene.instance()
+	if (vegtype < 2):
+		enemy.settype(vegtype)
+		rand.randomize()
+		var x = rand.randf_range(0, screen_size.x)
+		rand.randomize()
+		var y = 10
+		enemy.position.y = y
+		enemy.position.x = x
+	else:
+		rand.randomize()
+		if (randi()%2 == 0):
+			enemy.position.x = -30
+			direction = PI /2
+		else:
+			enemy.position.x = screen_size.x - 60
+			direction = -PI /2
+		enemy.position.y = rand_range(screen_size.y/8, (screen_size.y * 3)/4)
+	var velocity
+	if (vegtype == 0):
+		if (enemy.position.x > (screen_size.x / 2)):
+			direction = rand_range(PI / 8, PI / 4)
+		else:
+			direction = rand_range(-PI / 4, -PI / 8)
+		velocity = Vector2(0, rand_range(beet_spd_min, beet_spd_max))
+		enemy.linear_velocity = velocity.rotated(direction)
+	elif (vegtype == 1):
+		direction = 0
+		velocity = Vector2(0, rand_range(carrot_spd_min, carrot_spd_max))
+		enemy.linear_velocity = velocity.rotated(direction)
+	elif (vegtype == 2):
+		if (enemy.position.x < 0):
+			enemy._direction = 1
+		elif (enemy.position.x > 0):
+			enemy._direction = 0
+	add_child(enemy)
