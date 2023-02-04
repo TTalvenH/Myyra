5bedd851a7e49ed665b51e8492a646c893aed302
Linus Broms <lbroms@c1r1p5.hive.fi>
2023-02-04 17:26:45 +0200

    title screen and add ons

---
+++ FadeToBlack.gd
@@ -0,0 +1,11 @@
+extends ColorRect
+
+
+signal fade_finished
+
+func _ready():
+	$AnimationPlayer.play("FadeToBlack")
+
+
+func _on_AnimationPlayer_animation_finished(anim_name):
+	get_tree().change_scene("res://main.tscn")
