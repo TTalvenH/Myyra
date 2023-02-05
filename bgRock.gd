e9f5462ac0e019afd488067ba6f68ffbc3bd67d5
Joonas Henriksson <joonas.henriksson@gmail.com>
2023-02-05 15:06:15 +0200

    Background pixmap improvements

--- bgRock.gd
+++ bgRock.gd
@@ -7,8 +7,11 @@ extends Sprite
 
 var bgrock1 = preload("res://Sprites/bgrock1.png")
 var bgrock2 = preload("res://Sprites/bgrock2.png")
+var bgrock3 = preload("res://Sprites/bgrock3.png")
+var bgrock4 = preload("res://Sprites/bgrock4.png")
+var bgrock5 = preload("res://Sprites/bgrock5.png")
 
-var bgrock_sprites = [bgrock1, bgrock2]
+var bgrock_sprites = [bgrock1, bgrock2, bgrock3, bgrock4, bgrock5]
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	random_sprite()
